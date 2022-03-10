db6c697ad314ff95e7facb01f62d2a8b8506bc11
tanianasrin1 <tasrineva169@gmail.com>
2022-03-10 11:58:13 +0600

    image

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products)
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.images;
@@ -14,7 +15,7 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
